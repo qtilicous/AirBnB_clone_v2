d9e280cd797220b5c4e569a1b7f30d28086a48be
qtilicous <qtilicousbbz@gmail.com>
2024-02-16 04:15:36 -0800

    Add files of task 1 till 3

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+A Flask web application with routes for displaying messages.
+"""
+
+from flask import Flask, escape
+
+# Create an instance of the Flask application
+app = Flask(__name__)
+
+
+# Define a route for the root URL '/' that displays "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Display "Hello HBNB!" when the root URL is accessed.
+    """
+    return 'Hello HBNB!'
+
+
+# Define a route for the '/hbnb' endpoint that displays "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Display "HBNB" when the '/hbnb' endpoint is accessed.
+    """
+    return 'HBNB'
+
+
+# Define a route for the '/c/<text>' endpoint that displays "C <text>"
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Display "C <text>" when the '/c/<text>' endpoint is accessed.
+    Replace underscores (_) in <text> with spaces.
+    """
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+# Run the Flask application if the script is executed directly
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
