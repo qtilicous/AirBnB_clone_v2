c8600ce9d854682ba8bca3f24626b4181a726ac9
qtilicous <qtilicousbbz@gmail.com>
2024-02-18 08:47:14 -0800

    Fix files of task 3 and 4

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
 """
-A Flask web application with routes for displaying messages.
+This module starts a Flask web application.
 """
 
-from flask import Flask, escape
+from flask import Flask
 
-# Create an instance of the Flask application
 app = Flask(__name__)
 
 
-# Define a route for the root URL '/' that displays "Hello HBNB!"
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
-# Define a route for the '/hbnb' endpoint that displays "HBNB"
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Display 'HBNB'"""
     return 'HBNB'
 
 
-# Define a route for the '/c/<text>' endpoint that displays "C <text>"
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    return 'C {}'.format(escape(text).replace('_', ' '))
+    """Display 'C ', followed by the value of the text variable
+    (replace underscore _ symbols with a space)"""
+    return 'C ' + text.replace('_', ' ')
 
 
-# A route for the '/python/<text>' endpoint that displays "Python <text>"
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    return 'Python {}'.format(escape(text).replace('_', ' '))
+    """Display 'Python ', followed by the value of the text variable
+    (replace underscore _ symbols with a space)"""
+    return 'Python ' + text.replace('_', ' ')
 
 
-# Run the Flask application if the script is executed directly
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,38 +1,45 @@
 #!/usr/bin/python3
 """
-A flask web application that display messages
+This module starts a Flask web application.
 """
 
-from flask import Flask, escape
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
+def hbnb():
+    """Display 'HBNB'"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    return 'C {}'.format(escape(text).replace('_', ' '))
+def c_text(text):
+    """Display 'C ', followed by the value of the text variable
+    (replace underscore _ symbols with a space)"""
+    return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
-    return 'Python {}'.format(escape(text).replace('_', ' '))
+def python_text(text):
+    """Display 'Python ', followed by the value of the text variable
+    (replace underscore _ symbols with a space)"""
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def display_number(n):
+def is_number(n):
+    """Display 'n is a number' only if n is an integer"""
     return '{} is a number'.format(n)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
