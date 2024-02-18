ed9e1a0733f6676a98878dc2871bffc72c288598
qtilicous <qtilicousbbz@gmail.com>
2024-02-18 08:53:28 -0800

    Fix file 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,52 @@
 #!/usr/bin/python3
 """
-A Flask web application with routes for displaying messages.
+This module starts a Flask web application.
 """
 
-from flask import Flask, escape, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
+def hbnb():
+    """Display 'HBNB'"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    return 'C {}'.format(escape(text).replace('_', ' '))
+def c_text(text):
+    """Display 'C ', followed by the value of the text variable
+    (replace underscore _ symbols with a space)"""
+    return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
-    return 'Python {}'.format(escape(text).replace('_', ' '))
+def python_text(text):
+    """Display 'Python ', followed by the value of the text variable
+    (replace underscore _ symbols with a space)"""
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def display_number(n):
+def is_number(n):
+    """Display 'n is a number' only if n is an integer"""
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def display_number_template(n):
-    """
-    Display an HTML page with "Number: n" if n is an integer.
-    """
+def number_template(n):
+    """Display a HTML page only if n is an integer:
+    H1 tag: 'Number: n' inside the tag BODY"""
     return render_template('5-number.html', n=n)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
