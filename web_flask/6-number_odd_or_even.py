9aa4de0b2113ff1488b867bc5d8faef92404cb6c
qtilicous <qtilicousbbz@gmail.com>
2024-02-18 08:59:19 -0800

    6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application to respond to certain routes.
+This module starts a Flask web application.
 """
 
 from flask import Flask, render_template
@@ -10,39 +10,50 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Display 'HBNB'"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    return 'C {}'.format(text.replace('_', ' '))
+    """Display 'C ', followed by the value of the text variable
+    (replace underscore _ symbols with a space)"""
+    return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    return 'Python {}'.format(text.replace('_', ' '))
+    """Display 'Python ', followed by the value of the text variable
+    (replace underscore _ symbols with a space)"""
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_n(n):
+def is_number(n):
+    """Display 'n is a number' only if n is an integer"""
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('6-number_odd_or_even.html', n=n)
+    """Display a HTML page only if n is an integer:
+    H1 tag: 'Number: n' inside the tag BODY"""
+    return render_template('6-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
+    """Display a HTML page only if n is an integer:
+    H1 tag: 'Number: n is even|odd' inside the tag BODY"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
