1bc91f6d491aed1ea1d0205e266e8c7ae1605dcc
qtilicous <qtilicousbbz@gmail.com>
2024-02-18 08:41:01 -0800

    Fix file 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 """
-A Flask web application with routes for displaying messages.
+This module starts a Flask web application.
 """
 
-from flask import Flask, escape
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Display 'HBNB'"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    return 'C {}'.format(escape(text).replace('_', ' '))
+    """Display 'C ' followed by the value of the text variable
+    (replace underscore _ symbols with a space)"""
+    return 'C ' + text.replace('_', ' ')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
